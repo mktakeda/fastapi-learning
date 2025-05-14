af1e7adae93ddb3fa51826d9a210a27b2766f041
mktakeda <manav.khandurie@takeda.com>
2025-05-14 06:54:26 +0000

    learning GET Req

---
+++ src/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+database = {
+    1 : "MANAV",
+    2 : "SATVIK",
+    3 : "TANMAY"
+}
+
+@app.get("/health")
+def get_healthg():
+    return { 
+        "message" : "Healthy"
+    }
+
+@app.get("/user/{id}")
+def get_user(id : int):
+    user = database.get(id)
+    if user is None :
+        raise HTTPException( status_code = 404 , detail = 'NO USER FOUND')
+    return {
+        "name" : user
+    }
+
+@app.get("/{full_path:path}")
+def get_default_msg(full_path : str):
+    return {
+        "message" : f"DEFAULT PATH HIT for /{full_path}"
+    }
