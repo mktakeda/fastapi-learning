0845509aaebf8ea47b4005ea48572ba73fce542a
mktakeda <manav.khandurie@takeda.com>
2025-05-19 10:02:21 +0000

    RS256 integrated

--- src/config/config.py
+++ src/config/config.py
@@ -1,24 +1,28 @@
 # src/config.py
+from pathlib import Path
+
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     database_url: str = "sqlite:///./data/test.db"
-    public_key: str
-    private_key: str
+    public_key_path: str = "secrets/public.pem"
+    private_key_path: str = "secrets/private.pem"
     reload: bool = True
-    JWT_ALGORITHM: str = "HS256"
+    JWT_ALGORITHM: str = "RS256"
     JWT_EXPIRATION_MINUTES: int = 60
 
+    @property
+    def private_key(self):
+        return Path(self.private_key_path).read_text()
+
+    @property
+    def public_key(self):
+        return Path(self.public_key_path).read_text()
+
     class Config:
-        env_file = ".env"  # Path to your .env file
+        env_file = ".env"
         env_file_encoding = "utf-8"
 
 
 settings = Settings()
-
-"""
-DATABASE_URL = ''
-PUBLIC_KEY = ''
-PRIVATE_KEY = ''
-"""
--- src/security/auth/jwt_handler.py
+++ src/security/auth/jwt_handler.py
@@ -8,16 +8,14 @@ from src.config.config import settings
 def create_jwt(payload: dict) -> str:
     expire = datetime.utcnow() + timedelta(minutes=settings.JWT_EXPIRATION_MINUTES)
     payload.update({"exp": expire})
-    token = jwt.encode(
-        payload, "THIS IS A REPLACEMENT KEY", algorithm=settings.JWT_ALGORITHM
-    )
+    token = jwt.encode(payload, settings.private_key, algorithm=settings.JWT_ALGORITHM)
     return token
 
 
 def verify_jwt(token: str) -> str:
     try:
         decoded_token = jwt.decode(
-            token, "THIS IS A REPLACEMENT KEY", algorithms=[settings.JWT_ALGORITHM]
+            token, settings.public_key, algorithms=[settings.JWT_ALGORITHM]
         )
         return decoded_token
     except JWTError as e:
